2275792a38dd8240d303a51d622f062d70df3361
mateu <mateuscizeski@gmail.com>
2024-12-26 22:42:28 -0300

    Estrturua inicial da tabela.

---
+++ Domain/Entities/PointOfInterest.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities
+{
+    public sealed class PointOfInterest : Entity
+    {
+        public string Name { get; set; } = string.Empty;
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public PointOfInterest(string name, int x, int y)
+        {
+            if (x < 0 || y < 0) throw new NotValidDataException("The provided coordinates XY must be non-negative integers");
+            if (string.IsNullOrEmpty(name)) throw new NotValidDataException("A Point of Interest must have a NAME");
+
+            Name = name;
+            X = x;
+            Y = y;
+        }
+    }
+}
