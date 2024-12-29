1ad33c3cde845b133b600a5fe800ca8e785689c6
mateu <mateuscizeski@gmail.com>
2024-12-28 21:02:01 -0300

    estrutura da camada de dominio.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IGpsService, GpsService>();
+
+            return services;
+        }
+    }
+}
