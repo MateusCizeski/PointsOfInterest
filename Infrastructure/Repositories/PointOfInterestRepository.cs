0587f36f500413d92982c943131ca6a01baf10d8
mateu <mateuscizeski@gmail.com>
2024-12-30 20:27:50 -0300

    camada de infra.

--- Infrastructure/Repositories/PointOfInterestRepository.cs
+++ Infrastructure/Repositories/PointOfInterestRepository.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Core.Intefaces;
+using Domain.Entities;
+using System.Linq.Expressions;
 
 namespace Infrastructure.Repositories
 {
-    internal class PointOfInterestRepository
+    public class PointOfInterestRepository : IPointOfInterestRepository
     {
+        public Task<PointOfInterest> Add(PointOfInterest pointOfInterest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(PointOfInterest pointOfInterest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<PointOfInterest>> GetAllWhere(Expression<Func<PointOfInterest, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PointOfInterest> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
