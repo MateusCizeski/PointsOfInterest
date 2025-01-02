a04a69aced5cacee764997512872466cfe4cca40
mateu <mateuscizeski@gmail.com>
2025-01-02 19:55:32 -0300

    Métodos do controller.

--- PointsOfInterest/Controllers/GpsController.cs
+++ PointsOfInterest/Controllers/GpsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PointsOfInterest.Controllers
@@ -7,5 +9,56 @@ namespace PointsOfInterest.Controllers
     [ApiController]
     public class GpsController : ControllerBase
     {
+        private readonly IGpsService _gpsService;
+
+        public GpsController(IGpsService gpsService)
+        {
+            _gpsService = gpsService;
+        }
+
+        [HttpGet]
+        [Route("Near/x={x}/y={y}/max-distance={dmax}")]
+        public async Task<IActionResult> GetNearbyPoints(int x, int y, int dmax)
+        {
+            var nearbyPois = await _gpsService.GetNearbyPointsOfInterest(x, y, dmax);
+
+            return Ok(nearbyPois);
+        }
+
+        [HttpGet]
+        [Route("All")]
+        public async Task<IActionResult> GetAllPoints()
+        {
+            var points = await _gpsService.GetAllPointsOfInterest();
+
+            return Ok(points);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetPointOfInterestById(Guid id)
+        {
+            var poi = await _gpsService.GetPointOfInterestById(id);
+
+            return Ok(poi);
+        }
+
+        [HttpPost]
+        [Route("Create/Point")]
+        public async Task<IActionResult> CreatePoint(CreatePointOfInterestDTO dto)
+        {
+            var createdPoi = await _gpsService.CreatePointOfInterest(dto);
+
+            return CreatedAtAction(nameof(GetPointOfInterestById), new { id = createdPoi.Id }, createdPoi);
+        }
+
+        [HttpDelete]
+        [Route("Delete/{id}")]
+        public async Task<IActionResult> DeletePointOfInterest(Guid id)
+        {
+            await _gpsService.DeleteById(id);
+
+            return NoContent();
+        }
     }
 }
