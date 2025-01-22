9b4626cd660f5182e2027667318a43bf672ce186
mateu <mateuscizeski@gmail.com>
2025-01-22 20:04:10 -0300

    complementação de estruturas.

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+384f5e95512d7d9d4937337d6e291fb51e8b0b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c37c1a2b1da1791140d1819629076f738315aa6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
